fbb39e9d25e77a1912838ed5de3669a302eef1de
nelloodeluca <nelloodeluca@gmail.com>
2024-05-20 18:51:39 +0200

    index.tsx

--- UniCareer/app/index.tsx
+++ UniCareer/app/index.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+// In App.js in a new project
 
-const Home: React.FC = () => {
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function HomeScreen() {
     return (
-        <View>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>Home Screen</Text>
         </View>
     );
-};
+}
 
-export default Home;
+const Stack = createNativeStackNavigator();
+
+function App() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={HomeScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
+
+export default App;
