d0edb6b189bb3fd2cbeb05923af1fd7a5c7db8cd
nelloodeluca <nelloodeluca@gmail.com>
2024-05-21 20:05:36 +0200

    android packages

---
+++ UniCareer/Main.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { PaperProvider} from 'react-native-paper';
+import App from './App';
+
+export default function Main() {
+  return (
+    <PaperProvider>
+      <App />
+    </PaperProvider>
+  );
+}
