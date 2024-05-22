03312c1defd80602361b37be8a172da394fa3991
nelloodeluca <nelloodeluca@gmail.com>
2024-05-22 11:06:51 +0200

    cartelle

--- UniCareer/Main.tsx
+++ UniCareer/Main.tsx
@@ -1,12 +1,24 @@
 import * as React from 'react';
-import { Provider as PaperProvider } from 'react-native-paper';
+import {
+  MD3LightTheme as DefaultTheme,
+  PaperProvider,
+} from 'react-native-paper';
 import App from './App';
 
-console.log("Main component loaded");
+const theme = {
+  ...DefaultTheme,
+  // Specify custom property
+  myOwnProperty: true,
+  // Specify custom property in nested object
+  colors: {
+    ...DefaultTheme.colors,
+    myOwnColor: '#BADA55',
+  },
+};
 
 export default function Main() {
   return (
-    <PaperProvider>
+    <PaperProvider theme={theme}>
       <App />
     </PaperProvider>
   );
